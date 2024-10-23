bf936352b55abca4b33af8c8e7471a5d0572e296
Ariffaiz90 <payalthakur969491@gmail.com>
2024-10-23 23:21:04 +0530

    Update 0prem6.js

--- models/commands/0prem6.js
+++ models/commands/0prem6.js
@@ -3,7 +3,7 @@ module.exports.config = {
 	name: "welcome",
     version: "1.1.1",
 	hasPermssion: 0,
-	credits: "PREM BABU", 
+	credits: "FAIZU", 
 	description: "Just Respond",
 	commandCategory: "no prefix",
     cooldowns: 5, 
